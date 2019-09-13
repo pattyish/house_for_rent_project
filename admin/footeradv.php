b5e97d8bbc0bbc128cbe02c5b556745210d2eff3
pattyish <patrickishimwe10@gmail.com>
2019-09-13 12:00:45 +0200

    changes

---
+++ admin/footeradv.php
@@ -0,0 +1,11 @@
+<div class="container-fluid footer_admin bg-primary">
+                
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6" style="padding-top:15px; padding-bottom:12px;">
+                    <center><p ><i>Designed By &nbsp;&nbsp;House Mojor &copy; 2018</i></p></center>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+        
+    </body>
+</html>
---
+++ admin/public/includes/top_left_nav.php
@@ -0,0 +1,74 @@
+<style>
+ .nav-pills ul li{
+        color:white !important;
+    }
+    </style>
+   <nav class=" bg-primary">
+  <div class="container-fluid">
+      
+    <div class="navbar-header" style="padding-top:8px;">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php" style="font-size:35px;font-family:pristina; color:#fff; font-style:italic; font-weight:bold;"><strong>HOUSE</strong>&nbsp;<small style=
+      "color:orange;font-size:16px;">MAJOR</small></a>
+      </div>
+    <div class="nav navbar-nav navbar-right">
+        <div style="padding-top:16px; padding-bottom:16px; padding-right:30px;"><h4>Welcome <span style="color: orange;">Ngizwenayo Edson</span></h4></div>
+     
+    </div>
+  </div>
+</nav>
+
+    <div class="clearfix"></div>
+        <div class="container-fluid" style="margin-top: 2px;">
+       <div class="row">
+	    <div class="col-lg-2 well" style=" height:490px; ">
+		<div class="collapse navbar-collapse" id="myNavbar">
+                    <div class="">
+                <ul class="nav nav-pills nav-stacked" id="main-menu">
+
+                    <li>
+                        <a href="index.php" ><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="update_property.php"><i class="fa fa-pencil"></i> All Property</a>
+                    </li>
+                    <li>
+                        <a href="add_property_page.php"><i class="fa fa-gear"></i> Add Property</a>
+                    </li>
+    
+					<li>
+                        <a href="remove_property.php"><i class="fa fa-remove"></i> Remove Property</a>
+                    </li>
+					
+                    <li>
+                        <a href="confirm_request.php"><i class="fa fa-pencil"></i> Comfirm request</a>
+                    </li>
+                    <li>
+                        <a href="confirm_request.php"><i class="fa fa-user"></i> User</a>
+                    </li>
+
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-gear"></i> Settings
+                              <span class="caret"></span></a>
+                                   <ul class="dropdown-menu">
+                                 <li><a href="#">Profile</a></li>
+                                    <li><a href="#">Change Password</a></li>
+                                    <li><a href="#">Transaction</a></li>
+                                      </ul>
+                                        </li>
+					   <li>
+                        <a href="#"><i class="fa fa-user"></i> Logout </a>
+                    </li>
+                </ul>
+
+                </div>
+                </div>
+                </div>
+				
+				<!-----right side------>
+				
+               
