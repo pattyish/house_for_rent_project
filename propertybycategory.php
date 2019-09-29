494813129e1737408e9b91d960813df3723744fe
pattyish <patrickishimwe10@gmail.com>
2019-09-29 18:11:47 +0200

    propert categories
    the page you asked aman

---
+++ propertybycategory.php
@@ -0,0 +1,227 @@
+<?php include('public/includes/header.php')?>
+<?php include('public/includes/images_slider.php')?>
+    
+    <!---- end of horizontal silide --->
+    <div class="clearfix"></div>
+    <div class="container" id="properties_container">
+        <div class="row">
+               <div class=" col-md-3 col-sm-3 home_propety" style="">
+               <div class="card card-default">
+              <div class="card-header"></div>
+            <div class="card-body card-6-6">
+          <div class="card-left">
+             <img src="Properties_images/house_img2.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+           </div>
+          <div class="card-right">
+              <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+                </div>
+               </div>
+          <div class="card-body">
+           <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+         </div>
+          </div>
+           </div> 
+            <div class="col-sm-3 home_propety">
+      
+
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img1.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+       <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>     
+            <div class="col-sm-3 home_propety">
+      
+
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img3.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+        <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+     <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>     
+            <div class="col-sm-3 home_propety">
+      
+
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+     <img src="Properties_images/house_img4.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+        <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+  </div>
+</div>
+</div>   
+        
+        
+    <div class="col-md-3 home_propety">      
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img1.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+       <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>
+                <div class="col-md-3 home_propety">      
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img1.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+       <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>
+                <div class="col-md-3 home_propety">      
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img1.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+       <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>
+                <div class="col-md-3 home_propety">      
+<div class="card card-default">
+  <div class="card-header"></div>
+  <div class="card-body card-6-6">
+    <div class="card-left">
+      <img src="Properties_images/house_img1.jpg" class="img-responsive img-thumbnail" style="height:150px; width:100%;">
+    </div>
+    <div class="card-right">
+       <h5><a href="#">Commercial House for Rent in Nyarugenge - <span><strong>200 000/Rwf</strong></span></a></h5>
+    </div>
+  </div>
+  <div class="card-body">
+   <p class="">
+                   <label>Category:&nbsp;</label><span>Commercial</span><br>
+                    <label>Living Room:</label><span>&nbsp;2</span><br>
+                    <label>BedRooms:</label><span>&nbsp;7</span><br>
+                    <label>Indoor BathRoms:</label><span>&nbsp;2</span><br>
+                    <label>Outdoor BathRoms:</label><span>&nbsp;1</span><br>
+                    <label>Price:</label><span>&nbsp;200 000/Rwf</span><br>
+        
+               <i class="fa fa-map-marker">&nbsp;&nbsp;<a href=https://goo.gl/maps/JurHKa8bf5J8HV3p6 target="_blank">KN 245 St, Kigali</a></i>
+               <a href="#" class="btn btn-sm btn-link " style="float:right;"><i class="fa fa-list"></i>&nbsp;More details</a>
+              </p>
+    </div>
+</div>
+</div>
+</div>
+</div>
+        
+<?php include('public/includes/footer.php')?>
